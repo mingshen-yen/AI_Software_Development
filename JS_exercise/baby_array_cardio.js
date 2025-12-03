222e80517f9949008c5208647d50b989e8087b92
mingshen-yen <minghsuanyen@gmail.com>
2025-12-03 10:28:32 +0100

    js exercises

---
+++ JS_exercise/baby_array_cardio.js
@@ -0,0 +1,36 @@
+// Declare two variables, one with const and one with let
+// Give each one any value you'd like (string, number, array, object, boolean)
+
+// Log the values of each one of them to the console
+
+// Create an ES5 function with no parameters that prints a string to the console
+
+// Create an ES5 function that takes 2 parameters and **RETURNS** a string that includes those 2 parameters using template literal. Then log the value of that function to the console.
+
+// Transform the 2 ES5 functions from the previous exercises into ES6
+
+// Create an array containing 5 elements.
+
+// Log the first 2 values to the console
+
+// Log the last element in the array to the console
+
+// Log elements 3 and 4 in the array to the console
+
+// Create an object with the keys "name", "age", "occupation" and "hobbies" (hobbies should be an array)
+
+// Log the name to the console
+
+// Log the occupation and age to the console
+
+// Log all the hobbies to the console
+
+// Log only the first hobby to the console
+
+// Create an array containing 3 objects. The objects should have the same keys as the one in the previous exercise
+
+// Loop through the array of objects and log all the names to the console
+
+// Give 2 of your objects the same age. Now filter only the objects with that age.
+
+// Use the find method to display the last person in the array to the console.
